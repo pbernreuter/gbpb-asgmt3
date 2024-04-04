ebfbeb7ae356ddee79db6623568d8cdf7c4b2156
pokey136 <pbernreu@gmail.com>
2024-04-03 20:35:59 -0400

    added server code

---
+++ ftp_server.py
@@ -0,0 +1,44 @@
+from socket import * 
+import os
+
+# Create a TCP server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# Assign a port number and bind it to server socket
+serverSocket.bind(('', 6789))
+
+while True:
+    # Listen for 1 connection at a time
+    serverSocket.listen(1)  
+    # Set up a new connection from the client
+    clientSocket, addr = serverSocket.accept()
+
+    while True:
+        # Receive message from the client
+        message = clientSocket.recv(1024).decode()
+        message = message.split()
+
+        if message[0] == "LIST":
+            # Send the list of files in the current directory
+            file_list = '\n'.join(os.listdir('.'))
+            clientSocket.send(file_list.encode())
+        
+        elif message[0] == "RETRIEVE":
+            filename = message[1]
+            if os.path.exists(filename) and os.path.isfile(filename):
+                with open(filename, 'rb') as file:
+                    clientSocket.send(file.read())
+            else:
+                clientSocket.send("File not found".encode())
+
+        elif message[0] == "STORE":
+            filename = message[1]
+            file_data = clientSocket.recv(1024)
+            with open(filename, 'wb') as file:
+                file.write(file_data)
+
+        elif message[0] == "QUIT":
+            clientSocket.close()
+            break
+
+    
