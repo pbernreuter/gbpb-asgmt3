2d9ac7597d029c6ccda628bcff6db9866dbcb967
pokey136 <pbernreu@gmail.com>
2024-04-03 23:18:28 -0400

    Added Comments

--- ftp_client.py
+++ ftp_client.py
@@ -1,21 +1,34 @@
 from socket import *
 import os
 
+# Loop to keep the client running
 while True:
+    # Get user input to connect to the server
     user_input = input("Type: \"CONNECT <server_address> <server_port>\" to begin: ")
+
     if user_input.startswith("CONNECT"):
+        # Get the server address and port number from the user input 
         command, server_address, server_port = user_input.split()
         server_port = int(server_port)
+        # Create a TCP client socket
         client_socket = socket(AF_INET, SOCK_STREAM)
         client_socket.connect((server_address,server_port))
+        # Loop to keep the client running while connected to server
         while True:
+            # Get user input for the command to send to the server
             user_input = input("Enter command:")
+            
+            #end connection if user enters QUIT
             if user_input.startswith("QUIT"):
                 break
+
+            #send command and recieve and print server response
             elif user_input.startswith("LIST"):
                 client_socket.send(user_input.encode())
                 response = client_socket.recv(1024).decode()
                 print("Server response:\n", response)
+
+            #send filename to server and recieve and write file
             elif user_input.startswith("RETRIEVE"):
                 client_socket.send(user_input.encode())
                 user_input = user_input.split()
@@ -24,6 +37,8 @@ while True:
                 with open(filename, 'wb') as file:
                     file.write(response)
                 print("Retrieved " + filename + "from server.\n")
+
+            #send filename and file to server
             elif user_input.startswith("STORE"):
                 client_socket.send(user_input.encode())
                 user_input = user_input.split()
@@ -34,8 +49,12 @@ while True:
                     print(filename + " stored in server\n")
                 else:
                     print("File not found")
+            
+            #if user enters invalid command
             else:
                 print("Invalid Command")
+
+    #end connection if user enters QUIT and close socket
     if user_input.startswith("QUIT"):
         print("Disconnecting from server...")
         #send quit to server
--- ftp_server.py
+++ ftp_server.py
@@ -22,29 +22,39 @@ try:
             message = clientSocket.recv(1024).decode()
             message = message.split()
 
+            # List the files in the server directory
             if message[0] == "LIST":
                 # Send the list of files in the current directory
                 file_list = '\n'.join(os.listdir('.'))
                 clientSocket.send(file_list.encode())
             
+            # Retrieve the file requested by the client
             elif message[0] == "RETRIEVE":
+                #Get the filename from the message
                 filename = message[1]
+                #Check if the file exists in the server directory
                 if os.path.isfile(filename):
+                    #Send the file to the client
                     with open(filename, 'rb') as file:
                         clientSocket.send(file.read())
                 else:
                     clientSocket.send("File not found".encode())
 
+            # Store the file sent by the client
             elif message[0] == "STORE":
+                #Get filename and file data
                 filename = message[1]
                 file_data = clientSocket.recv(1024)
+                #Write the file data to the server directory
                 with open(filename, 'wb') as file:
                     file.write(file_data)
 
+            # Close the connection if the client sends a QUIT message 
             elif message[0] == "QUIT":
                 clientSocket.close()
                 break
 
+# Close the server socket and terminate the program
 except KeyboardInterrupt:
     print("Server terminated by user.")
     serverSocket.close()
